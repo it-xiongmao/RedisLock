2107ad7f22b9e13621b5517dff655928b07acff7
BRUCELIU9527 <1241488705@qq.com>
2019-07-16 22:56:44 +0800

    添加工程到GIT

---
+++ GitTest/src/main/java/com/bruceliu/test/Test.java
@@ -0,0 +1,15 @@
+package com.bruceliu.test;
+
+/**
+ * @author bruceliu
+ * @create 2019-07-16 22:42
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("GIT测试");
+        System.out.println("GIT测试");
+        System.out.println("GIT测试");
+        System.out.println("GIT测试");
+    }
+}
